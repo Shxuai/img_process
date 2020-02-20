cbe44d5f0ccf8c7ec6bd3a8a47ef6f09b2bb7ff5
Shxuai <769804622@qq.com>
2020-02-20 23:57:40 +0800

    no message

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\Shxuai\Documents\GitHub\Shxuai_git\Python\img_process\venv"
+$env:VIRTUAL_ENV="F:\img_process\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
