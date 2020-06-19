30c0d0ac89583c3f1db1afb4db421e0cc7992025
Shxuai <769804622@qq.com>
2020-06-19 17:40:27 +0800

    no message

--- learning_process.py
+++ learning_process.py
@@ -1,8 +1,13 @@
 import numpy as np
 import torch
 import os
+import time
+
+from tqdm import trange
 
 import train_data_process
+
+CHUNK_SIZE = train_data_process.CHUNK_SIZE
 
 img_data = [[], []]
 
@@ -13,9 +18,53 @@ for root, dirs, files in os.walk("resource\img_data", topdown=False):
         elif os.path.join(root, name).endswith('.pixel'):
             img_data[1].append(os.path.join(root, name))
 
+chunk = []
+pixel = []
+
 for i in range(len(img_data[0])):
-    chunk = np.fromfile(img_data[0][i], dtype=np.uint8)
-    chunk.resize(train_data_process.CHUNK_SIZE, train_data_process.CHUNK_SIZE, 25)
-    pixel = np.fromfile(img_data[1][i], dtype=np.uint8)
-    print(pixel)
-    exit()
+    chunk.append(np.fromfile(img_data[0][i], dtype=np.uint8))
+    # chunk[i].resize(CHUNK_SIZE, CHUNK_SIZE, 25)
+    local_pixel = np.fromfile(img_data[1][i], dtype=np.uint8).tolist()
+    # print(type(local_pixel))
+    # exit()
+    for j in range(len(local_pixel)):
+        local_pixel[j] = train_data_process.binary_encode(local_pixel[j], train_data_process.NUM_DIGITS)
+    local_pixel = np.array(local_pixel).flatten()
+    pixel.append(local_pixel)
+
+trX = torch.Tensor(chunk)
+trY = torch.Tensor(pixel)
+del chunk, pixel
+print(trX.shape)
+
+# Define the model
+NUM_HIDDEN = 100
+model = torch.nn.Sequential(
+    torch.nn.Linear(CHUNK_SIZE * CHUNK_SIZE * 25, CHUNK_SIZE * 25),
+    torch.nn.ReLU(),
+    torch.nn.Linear(CHUNK_SIZE * 25, 25),
+    torch.nn.Linear(25, 24),
+)
+
+loss_fn = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.05)
+
+# Start training it
+print('Start training!')
+BATCH_SIZE = 128
+for epoch in range(10000):
+    for start in range(0, len(trX), BATCH_SIZE):
+        end = start + BATCH_SIZE
+        batchX = trX[start:end]
+        batchY = trY[start:end]
+
+        y_pred = model(batchX)
+        loss = loss_fn(y_pred, batchY)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    # Find loss on training data
+    loss = loss_fn(model(trX), trY).item()
+    print('Epoch:', epoch, 'Loss:', loss)
